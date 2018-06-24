a1f5b96789a083354ff0004ff63e92635a87f2e8
alexandru.fodor <afodor88@gmail.com>
2018-06-24 21:42:58 +0300

    added monochrome for more nicer reading of errors

--- src/test/java/runner/MainRunner.java
+++ src/test/java/runner/MainRunner.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"src/test/java/featureFiles"},
+        monochrome = true,
         glue = {"stepsFiles"},
         plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json", "com.cucumber.listener.ExtentCucumberFormatter:target/report.html"
         }
